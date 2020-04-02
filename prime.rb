943fb5d383434c7625c7343aeda7fc890eec0898
Evan Smith <professionalemail@zoho.com>
2020-04-02 19:46:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,8 @@
 def prime?(number)
   if number < 2
     return false
-  elsif number == 2
-    return true
     else
-    array = (2..number).to_a
+    array = (2..number-1).to_a
     array.none? do |factor|
     number % factor == 0
     end
