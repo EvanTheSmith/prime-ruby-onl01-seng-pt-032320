bf9e6eacbbac7ae5180f97002cd20199e16f3991
Evan Smith <professionalemail@zoho.com>
2020-04-02 19:40:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,7 @@ def prime?(number)
   if number < 0
     return false
   else
-    array = (2..number).to_a
-    array.none? do |factor|
-      number % factor == 0
-    end
+    (2..number).none? do |factor|
+    number % factor == 0
   end
 end
